e6ea4bae436edce69c34372e02d571c91fc5d22b
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-09 16:38:22 +0530

    coursel working

--- Frontend/src/customer/components/HomeCarousel/MainCarousel.jsx
+++ Frontend/src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-import { MainCarouselData } from "./MainCarouselData";
+import { MainCarouselData } from "./MainCarouselData.jsx";
 
 const MainCarousel = () => {
   const items = MainCarouselData.map((item) => (
-    <img className="cursor-pointer" role="presentation" src="item.image" />
+    <img className="cursor-pointer" role="presentation" src={item.image} />
   ));
 
   return <AliceCarousel items={items} />;
--- Frontend/src/customer/components/HomeCarousel/MainCarouselData.jsx
+++ Frontend/src/customer/components/HomeCarousel/MainCarouselData.jsx
@@ -1,7 +1,7 @@
 export const MainCarouselData = [
   {
-    image:
-      "https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/2/5/25_dresses_1_.jpg",
+    image:"https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/8/l/8lehenga_coll_copy.jpg_mob_2_.jpg",
+      
     path: "/women/clothing/lengha_choli",
   },
   {
@@ -10,8 +10,8 @@ export const MainCarouselData = [
     path: "/women/clothing/womendress",
   },
   {
-    image:
-      "https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/8/l/8lehenga_coll_copy.jpg_mob_2_.jpg",
+    image:"https://d6kigqwjl9u8w.cloudfront.net/mageplaza/bannerslider/banner/image/2/5/25_dresses_1_.jpg",
+      
     path: "/women/clothing/women-saree",
   },
 ];
