a82d4585fc242e52e9cb5f0bd0b9c68f112082dc
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-09 23:33:39 +0530

    cards added

--- Frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ Frontend/src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
+import { Button } from "@mui/material";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
-
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 const HomeSectionCarousel = () => {
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
   };
-  const items = [11, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+  const items = [1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
   return (
     <div className="relative px-4 lg:px-8">
         <div className="relative p-5">
@@ -21,6 +22,9 @@ const HomeSectionCarousel = () => {
         items={items}
 
       />
+  <Button  variant="contained" sx={{position:"absolute",top:"8rem",right:"0rem", transfrom:""}} aria-label="next"   className="z-50">
+    <KeyboardArrowLeftIcon/>
+  </Button>
       ;</div>
     </div>
   );
