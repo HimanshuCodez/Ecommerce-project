eb155e0b4b553ec579117564a5556056199b288d
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-09 10:04:39 +0530

    removed files

--- Frontend/src/pages/shopping-view/listing.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ShoppingListing = () => {
-  return (
-    <div>ShoppingListing</div>
-  )
-}
-
-export default ShoppingListing
--- Frontend/src/pages/unauth-page/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UnauthPage = () => {
-  return (
-    <div>UnauthPage</div>
-  )
-}
-
-export default UnauthPage
