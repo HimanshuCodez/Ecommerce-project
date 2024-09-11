99c37906edb09fb9863b36b5a7f3754c0ace1b05
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-11 19:53:07 +0530

    product cards

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Navigation from './customer/components/Navigation/Navigation.jsx'
 import HomePage from './customer/pages/HomePage/HomePage.jsx'
 import Footer from './customer/components/Footer/Footer.jsx'
+import Product from './customer/components/Product/Product.jsx'
 
 const App = () => {
   return (<>
   <div>
  <Navigation/>
- <HomePage/>
+ {/* <HomePage/> */}
+ <Product/>
 <Footer/>
     </div></>
    
---
+++ Frontend/src/customer/components/Product/ProductCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ProductCard.css"
+const ProductCard = () => {
+  return (
+    <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
+      <div className="h-[20rem]">
+        <img
+          className="h-full w-full object-cover object-left-top "
+          src="https://images.meesho.com/images/products/397495332/d3nfc_400.webp"
+          alt=""
+        />
+      </div>
+      <div className="textPart bg-white p-3">
+        <div>
+          <p className='font-bold opacity-60'>Universal Outfit</p>
+          <p >Casual Puff Butterfly top</p>
+        </div>
+        <div className=" flex items-center  space-x-2">
+<p className='font-semibold'>₹199</p>
+<p className=' line-through opacity-50' >₹1999</p>
+<p  className=' text-green-600 font-semibold'>70% off</p>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
