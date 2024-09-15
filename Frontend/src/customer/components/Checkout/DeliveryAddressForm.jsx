b83db60dd5ef8f53541bff237871a6b48dd9e90f
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-15 13:52:23 +0530

    deliver form added

--- Frontend/src/customer/components/Checkout/DeliveryAddressForm.jsx
+++ Frontend/src/customer/components/Checkout/DeliveryAddressForm.jsx
@@ -1,21 +1,156 @@
-import { Button, Grid2 } from '@mui/material'
-import React from 'react'
-import AddressCard from '../AddressCard/AddressCard.jsx'
+import { Box, Button, Grid, TextField } from "@mui/material";
+import React from "react";
+import AddressCard from "../AddressCard/AddressCard.jsx";
 
 const DeliveryAddressForm = () => {
+  const handleSubmit=(e)=>{
+    // default behaviour submit hote hi refresh ho jata h 
+    e.preventDefault()
+    console.log("address")
+    const data =new FormData(e.currentTarget)
+    const address={
+      firstName:data.get("firstName"),
+      lastName:data.get("lastName"),
+      streetAddress:data.get("streetAddress"),
+      city:data.get("city"),
+      state:data.get("state"),
+      zipCode:data.get("zip"),
+      mobile:data.get("phoneNumber"),
+
+    }
+    console.log("address",address)
+  }
   return (
     <div>
-        <Grid2 container spacing={4}>
-            <Grid2 className='border rounded-e-md shadow-md h-[30.5rem] overflow-y-scroll'>
-                <div className='py-7 p-5 border-b cursor-pointer '>
-<AddressCard/>
-<Button sx={{mt:2,bgcolor:"RGB(145 85 253)"}} size='large' variant='contained'>Deliver Here</Button>
-                </div>
+      <Grid container spacing={4}>
+        {/* Address Card Section */}
+        <Grid
+          item
+          xs={12}
+          lg={5}
+          sx={{
+            border: 1,
+            borderColor: "divider",
+            borderRadius: "0 8px 8px 0",
+            boxShadow: 2,
+            height: "30.5rem",
+            overflowY: "auto",
+          }}
+        >
+          <Box sx={{ py: 4, px: 3, borderBottom: 1, borderColor: "divider" }}>
+            <AddressCard />
+            <Button
+              sx={{ mt: 2, bgcolor: "RGB(145 85 253)" }}
+              size="large"
+              variant="contained"
+            >
+              Deliver Here
+            </Button>
+          </Box>
+        </Grid>
 
-            </Grid2>
-        </Grid2>
+        {/* Delivery Address Form Section */}
+        <Grid item xs={12} lg={7}>
+          <Box
+            sx={{
+              border: 1,
+              borderColor: "divider",
+              borderRadius: "8px",
+              boxShadow: 2,
+              padding: 4,
+            }}
+          >
+            <form onSubmit={handleSubmit}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="firstName"
+                    name="firstName"
+                    label="First Name"
+                    fullWidth
+                    autoComplete="given-name"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="lastName"
+                    name="lastName"
+                    label="Last Name"
+                    fullWidth
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    id="address"
+                    name="address"
+                    label="Address"
+                    fullWidth
+                    multiline
+                    rows={4}
+                    autoComplete="street-address"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    id="city"
+                    name="city"
+                    label="City"
+                    fullWidth
+                    autoComplete="street-address"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    id="state"
+                    name="state"
+                    label="State/Province/Region"
+                    fullWidth
+                    autoComplete="street-address"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    id="zip"
+                    name="zip"
+                    label="Zip / Postal code"
+                    fullWidth
+                    autoComplete="shipping postal-code "
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    id="phonenumber"
+                    name="phonenumber"
+                    label="Phone Number"
+                    fullWidth
+                    autoComplete="street-address"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    sx={{ py:1.5, mt: 2, bgcolor: "RGB(145 85 253)" }}
+                    size="large"
+                    variant="contained"
+                    type='submit'
+                  >
+                    Deliver Here
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default DeliveryAddressForm
+export default DeliveryAddressForm;
