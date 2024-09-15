bc19e05d928c5f898ad64f9384d07de78e0d0471
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-15 15:27:05 +0530

    order summary page added

--- Frontend/src/customer/components/AddressCard/AddressCard.jsx
+++ Frontend/src/customer/components/AddressCard/AddressCard.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const AddressCard = () => {
   return (
-    <div>AddressCard</div>
-  )
-}
+    <div className="p-4 bg-white rounded-md  space-y-3">
+      <p className="text-lg font-semibold">Mintu Singh</p>
+      <p className="text-gray-600">Delhi, Gokul Dham Market, 40001</p>
+      <div className="space-y-1">
+        <p className="text-sm font-semibold">Phone number</p>
+        <p className="text-gray-800">9392912129</p>
+      </div>
+    </div>
+  );
+};
 
-export default AddressCard
+export default AddressCard;
