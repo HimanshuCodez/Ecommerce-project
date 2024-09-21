50e9e9546a5353ac2dd080eb3388035a33c08ca1
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-21 21:48:01 +0530

    jwt services

--- Backend/src/services/user.services.js
+++ Backend/src/services/user.services.js
@@ -1,19 +1,57 @@
 import User from "../models/user.model"
-import bcrypt    from  'bcrypt'
-const createUser=async(userData)=>{
+import bcrypt from 'bcrypt'
+const createUser = async (userData) => {
     try {
-        let {firstName,lastName,email,password} =userData
-        const isUserExist=await User.findOne({email})
+        let { firstName, lastName, email, password } = userData
+        const isUserExist = await User.findOne({ email })
 
-        if(isUserExist){
-            throw new Error("user already exists with email",email);
+        if (isUserExist) {
+            throw new Error("user already exists with email", email);
         }
-        password=await bcrypt.hash(password,8)
-        const user=await User.create ({firstName,lastName,email,password})
-        console.log("user created");
-        
+        password = await bcrypt.hash(password, 8)
+        const user = await User.create({ firstName, lastName, email, password })
+        console.log("user created", user);
+
+        return user
+
+    } catch (error) {
+        throw new Error(error.message);
+
+    }
+}
+const findUserById = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        if (!user) {
+            throw new Error("user not found by id ", userId);
+        }
         return user
     } catch (error) {
-        
+        throw new Error(error.message);
+
     }
+
+
+
+}
+const findUserByEmail = async (email) => {
+    try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            throw new Error("user not found by email ", email);
+        }
+        return user
+    } catch (error) {
+        throw new Error(error.message);
+
+    }
+}
+//jwt
+const  getUserProfileByToken = async (token)=>{
+
+}
+export {
+    createUser,
+    findUserById,
+    findUserByEmail
 }
