bc2885f46714be2c43b1a606fa401c062413ba33
Himanshu gaur <himanshugaur055@gmail.com>
2024-09-23 13:12:04 +0530

    login register

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,7 +1,19 @@
 import {userService} from '../services/user.services.js'
 import {jwtProvider} from '../services/jwtProvider.js'
 const register =async(req,res) =>{
+try {
+    const user =await userService.createUser(req.body)
+    const jwt=jwtProvider.generateToken(user._id);
+//user ke create hote hi cart bhi create ho jaye
+    // await cartService.createCart(user);
+    return res.return(200).send({jwt,message:"register success"})
 
+
+} catch (error) {
+    
+return res.status(500).send({error:error.message});
+
+}
     
     
 }
